0a293de70a7f1d9ced2260cc53c16a7cb793e583
ruwik <manindrakrishna@gmail.com>
2018-08-10 19:26:10 +0530

    Added functions for square and cube

--- src/basic.js
+++ src/basic.js
@@ -20,13 +20,17 @@ const isOdd = function(number){
   This function takes a number and returns the mathematical square
   of that number
 */
-
+const square = function(number){
+    return number * number;
+};
 /*
   cube
   This function takes a number and returns the mathematical cube
   of that number
 */
-
+const cube = function(number){
+    return number*number*number;
+};
 
 /*
   gcd
